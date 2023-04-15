c312fa8e45d771ae8e09fad52b4034c028c9e275
luch4736 <89386521+luch4736@users.noreply.github.com>
2023-04-15 16:11:19 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "luch4736's CloudDrive",
+  title: "luch4736's Cloud",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
